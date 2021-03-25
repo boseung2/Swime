e599aaf8eb502309c6d471b768fd213e5ea52b50
toywar94 <toywar94@gmail.com>
2021-03-25 19:47:24 +0900

    Add studymapper Modify Variable name of studyVO Modify studymapper.xml

--- src/main/java/com/swime/domain/StudyVO.java
+++ src/main/java/com/swime/domain/StudyVO.java
@@ -9,18 +9,18 @@ public class StudyVO {
 
     private Long stdNum;
     //fk
-    private Long GrpNum;
+    private Long grpNum;
     private Long stdPlcNum;
 
-    private String stdName;
-    private Date stdStartTime;
-    private Date stdEenTime;
-    private String stdExp; //가격
-    private int stdCap; //인원
-    private String stdInfo; //정보
-    private String stdRep; //스터디 장
-    private Date stdRegdate;
-    private int stdStatus;
+    private String name;
+    private String startTime;
+    private String endTime;
+    private String exp; //가격
+    private int cap; //인원
+    private String info; //정보
+    private String rep; //스터디 장
+    private Date regdate;
+    private int status;
 
 
 
--- src/main/java/com/swime/mapper/StudyMapper.java
+++ src/main/java/com/swime/mapper/StudyMapper.java
@@ -1,4 +1,8 @@
 package com.swime.mapper;
 
+import com.swime.domain.StudyVO;
+
 public interface StudyMapper {
+
+    public int insert(StudyVO vo);
 }
