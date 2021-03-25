c4372262bf8e01177f12d580d13524db41fc66a0
Jung <tht1234551@gmail.com>
2021-03-25 11:18:56 +0900

    login

---
+++ src/test/java/com/swime/persistence/JdbcTests.java
@@ -0,0 +1,35 @@
+package com.swime.persistence;
+
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static org.junit.Assert.fail;
+
+public class JdbcTests {
+    static {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+	@Test
+	public void testConnection() {
+		try(Connection con =
+				DriverManager.getConnection(
+						"jdbc:oracle:thin:@aiaclassi1.iptime.org:3000:XE",
+						"swime",
+						"1234")){
+            System.out.println(con);;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
+
