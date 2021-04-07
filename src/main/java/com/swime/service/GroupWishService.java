d30cfa8776f10826414174d2a2845a0bb5a9429c
Jung <tht1234551@gmail.com>
2021-04-07 20:04:14 +0900

    User GroupWishList #2 Write service #1

--- src/main/java/com/swime/service/GroupWishService.java
+++ src/main/java/com/swime/service/GroupWishService.java
@@ -1,12 +1,21 @@
 package com.swime.service;
 
 
+import com.swime.domain.GroupWishVO;
 import com.swime.domain.MemberHistoryVO;
 import com.swime.domain.MemberVO;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface GroupWishService {
 
+    List<GroupWishVO> readByGroupSn(long grpSn);
+
+    List<GroupWishVO> readByid(String id);
+
+    boolean insert(GroupWishVO vo);
+
+    boolean delete(@Param("id") String id, @Param("grpSn") long grpSn);
 
 }
