c299022a2f2c1d73ed692801a11c24f362073e34
Jung <tht1234551@gmail.com>
2021-04-07 20:39:12 +0900

    GmailSend #1 Test send #1

--- src/test/java/com/swime/util/GmailSendTest.java
+++ src/test/java/com/swime/util/GmailSendTest.java
@@ -1,4 +1,18 @@
 package com.swime.util;
 
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {com.swime.config.RootConfig.class})
+@Log4j
 public class GmailSendTest {
+
+    @Test
+    public void test(){
+        new GmailSend();
+    }
 }
