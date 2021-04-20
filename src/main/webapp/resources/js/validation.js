a9155622f3d664c83c92ea1e4a3c0053e6f97ded
Jung <tht1234551@gmail.com>
2021-04-20 14:06:59 +0900

    profile...

---
+++ src/main/webapp/resources/js/validation.js
@@ -0,0 +1,7 @@
+// if(getByte($('#name').val()) == "") {
+//     alert("모임명을 입력해주세요");
+//     return false;
+// } else if(getByte($('#name').val()) > 70) {
+//     alert("모임명을 20자 이하로 작성해주세요");
+//     return false;
+// }
