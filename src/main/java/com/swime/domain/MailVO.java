dbcc0022d74114378932098e9fdfb6abec52dbe7
boseung2 <jungbs3726@google.co.kr>
2021-05-11 11:51:43 +0900

    before merge

--- src/main/java/com/swime/domain/MailVO.java
+++ src/main/java/com/swime/domain/MailVO.java
@@ -8,14 +8,18 @@ public class MailVO {
     String Text = "";
     String receiver = "";
 
+    public MailVO(String receiver, String subject, String text){
+        this.receiver = receiver;
+        this.Subject = subject;
+        this.Text = text;
+    }
+
     public MailVO(String receiver, String key){
         this(receiver,"swime 인증메일입니다","안녕하세요 다음 url을 클릭해서 인증을 마치세요\n"
                 +"http://localhost/user/auth" + "?id=" + receiver + "&key=" + key);
     }
 
-    public MailVO(String receiver, String subject, String text){
-        this.receiver = receiver;
-        this.Text = text;
-        this.receiver = receiver;
-    }
+//    private String makeContent(String receiver, String key) {
+//        String content =
+//    }
 }
