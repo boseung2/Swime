633a3a8cd539ff7fa5b7955a90a5cc20211ee176
Jung <tht1234551@gmail.com>
2022-07-19 21:49:26 +0900

    jwt 라이브러리 추가, 유틸 추가

---
+++ src/main/java/com/swime/util/JwtUtil.java
@@ -0,0 +1,114 @@
+package com.swime.util;
+
+import com.swime.domain.MemberVO;
+import com.swime.security.CustomUser;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Base64;
+import java.util.Date;
+
+public class JwtUtil {
+
+        public static final String ACCESS_TOKEN = "ACCESS_TOKEN";
+        public static final String REFRESH_TOKEN = "REFRESH_TOKEN";
+        public final Long accessValidity;
+        public final Long refreshValidity;
+        private final String secretKey;
+
+        public JwtUtil(
+                String secretKey,
+               Long accessValidity,
+               Long refreshValidity
+        ) {
+            this.secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+            this.accessValidity = accessValidity;
+            this.refreshValidity = refreshValidity;
+        }
+
+        public String createAccessToken(MemberVO vo) {
+            return createToken(vo, ACCESS_TOKEN, accessValidity);
+        }
+//
+//        public String createRefreshToken(UserTokenDto userTokenDto) {
+//            return createToken(userTokenDto, REFRESH_TOKEN, refreshValidity);
+//        }
+//
+        public String createToken(MemberVO vo, String tokenType, Long validity) {
+            Claims claims = Jwts.claims();
+            claims.put("tokenType", tokenType);
+            claims.put("userId", vo.getId().toString());
+//            claims.put("role", vo.getRole());
+
+            Date now = new Date();
+            Date expiration = new Date(now.getTime() + validity);
+
+            return Jwts.builder()
+                    .setClaims(claims)
+                    .setIssuedAt(now)
+                    .setExpiration(expiration)
+                    .signWith(SignatureAlgorithm.HS256, secretKey)
+                    .compact();
+        }
+//
+//        private Claims getClaims(String token) {
+//            return Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+//        }
+//
+//        public UserTokenDto getUserTokenDto(String token) {
+//            Claims claims = getClaims(token);
+//            return UserTokenDto.builder()
+//                    .id(Long.valueOf(claims.get("userId", String.class)))
+//                    .role(Role.valueOf(claims.get("role", String.class)))
+//                    .build();
+//        }
+//
+//        public Authentication getAuthentication(String token) {
+//            UserTokenDto userTokenDto = getUserTokenDto(token);
+//            return new UsernamePasswordAuthenticationToken(userTokenDto.getId(), null,
+//                    Collections.singleton(new SimpleGrantedAuthority(userTokenDto.getRole().toString())));
+//        }
+//
+//        public String getTokenFromRequest(HttpServletRequest request) {
+//            String token = request.getHeader("Authorization");
+//            return token == null ? null : token.substring("Bearer ".length());
+//        }
+//
+//        public void validateToken(String token) {
+//            try {
+//                Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+//            } catch (SignatureException | MalformedJwtException | UnsupportedJwtException | IllegalArgumentException e) {
+//                throw new InvalidTokenException("유효하지 않은 토큰입니다.");
+//            } catch (ExpiredJwtException e) {
+//                throw new ExpiredTokenException("이미 만료된 토큰입니다.");
+//            }
+//        }
+//
+//        public Long getRefreshValidity() {
+//            return refreshValidity;
+//        }
+//
+//        public boolean isRefreshToken(String token) {
+//            return getClaims(token).get("tokenType").equals(REFRESH_TOKEN);
+//        }
+}
+
+//@Data
+//@Builder
+//class UserTokenDto {
+//
+//    private String id;
+//
+//    private String role;
+//
+//    public UserTokenDto from(MemberVO user) {
+//        return UserTokenDto.builder()
+//                .id(user.getId())
+//                .role(user.getRole())
+//                .build();
+//    }
+//}
